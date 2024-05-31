b9442e185959aae3b7bfc9a53836ba0a535448a7
YejiAhn <yejilove9001@naver.com>
2024-05-31 13:15:20 +0900

    first commit

--- SmartParkingSystem.ino
+++ SmartParkingSystem.ino
@@ -1,116 +1,58 @@
-#include<Servo.h>
-#include<LiquidCrystal_I2C.h>
-Servo myservo;
+#include <LiquidCrystal_I2C.h>
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-int carNumber = 0;
-int pos = 0;
-int servoPin = 6;
-
-#define TRIG 9
-#define ECHO 8
-
-int RED = 12;
-int GREEN = 11;
-int BLUE = 10;
-bool barrierRaised = false;
-
 void setup() {
-    //초음파
-    Serial.begin(9600);
-    pinMode(TRIG, OUTPUT);
-    pinMode(ECHO, INPUT);
 
-    //서보모터
-    pinMode(servoPin, OUTPUT);
-    myservo.attach(6);
-    myservo.write(0);
+  Serial.begin(9600);
+  pinMode(A0, INPUT); // 조도센서 1
+  pinMode(A1, INPUT); // 조도센서 2
+  pinMode(A2, INPUT); // 조도센서 3  
 
-    //조도센서
-    pinMode(A0, INPUT); //들어오는 차 감지
-    pinMode(A1, INPUT); //나가는 차 감지
-
-    //3색 led
-    pinMode(RED, OUTPUT);
-    pinMode(GREEN, OUTPUT);
-    pinMode(BLUE, OUTPUT);
 }
+
 void loop() {
-    //초음파 센서
-    long duration, distance;
+  // 조도센서: 차량 주차 여부 확인
+  int N0 = analogRead(A0);
+  Serial.print("N0: ");
+  Serial.println(N0);
+  delay(10);
 
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
+  int N1 = analogRead(A1);
+  Serial.print("N1: ");
+  Serial.println(N1);
+  delay(10);
 
-    duration = pulseIn(ECHO, HIGH);
-    distance = duration * 17 / 1000;
+ int N2 = analogRead(A2);
+  Serial.print("N2: ");
+  Serial.println(N2);
 
-    Serial.println(duration);
-    Serial.print("\nDistance:");
-    Serial.print(distance);
-    Serial.println("Cm");
-
-    // 차단봉 동작 코드
-    if (carNumber >= 5) {
-        if (barrierRaised) {
-            myservo.write(0);
-            delay(1000);
-            barrierRaised = false;
-        }
-    }
-    else if (!barrierRaised && distance <= 10) {
-        for (pos = 0; pos <= 90; pos += 1) {
-            myservo.write(pos);
-            delay(10);
-        }
-        delay(1000);
-        for (pos = 90; pos >= 0; pos -= 1) {
-            myservo.write(pos);
-            delay(10);
-        }
-        barrierRaised = false;
-    }
-    //조도센서
-
-    int comein = analogRead(A0);
-    Serial.println("Come in : " + String(comein));
-    delay(100);
-    int goout = analogRead(A1);
-    Serial.println("Go out : " + String(goout));
-    delay(1000);
-
-    if (comein < 120) {
-        carNumber = carNumber + 1;
-    }
-    if (goout < 120) {
-        carNumber = carNumber - 1;
-    }
-    //lcd
+  // LCD 출력
   lcd.begin();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Car Number :");
-  lcd.print(carNumber);
-
-  //3색 led
-  if(carNumber <=4){
-    analogWrite(RED,0);
-    analogWrite(GREEN,200);
-    analogWrite(BLUE, 0);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  // 1번째 조도센서
+  lcd.print("N0:");
+  if (N0 < 100) {
+    lcd.print("X ");
+  } else {
+    lcd.print("O ");
   }
-  if(carNumber >=5){
-    analogWrite(RED,200);
-    analogWrite(GREEN,0);
-    analogWrite(BLUE, 0);
+  
+// 2번째 조도센서
+  lcd.print("N1:");
+  if (N1 < 100) {
+    lcd.print("X ");
+  } else {
+    lcd.print("O ");
+    }
+  // 3번째 조도센서
+  lcd.print("N2:");
+  if (N2 < 100) {
+    lcd.print("X ");
+  } else {
+    lcd.print("O ");
+  }  
+  lcd.setCursor(0, 1);
+  lcd.print("Welcome!");
   }
-
-  delay(1000);  
-}
-
-
-
-
-
-
